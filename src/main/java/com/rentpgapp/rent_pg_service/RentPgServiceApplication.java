53f941aec97c380a751f554c8ae668c0f0b332ed
Tanzeem <tanzeembelal2@gmail.com>
2025-06-21 02:11:47 +0530

    Boilerplate code added in main branch

---
+++ src/main/java/com/rentpgapp/rent_pg_service/RentPgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rentpgapp.rent_pg_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentPgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentPgServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentpgapp/rent_pg_service/RentPgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentpgapp.rent_pg_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentPgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
