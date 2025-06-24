5c335a16378b47554dd2eb4912193c44b0f056db
Tanzeem <tanzeembelal2@gmail.com>
2025-06-25 02:42:43 +0530

    Users Entity with Roles Enum added

--- src/main/java/com/rentpgapp/rent_pg_service/model/Booking.java
+++ src/main/java/com/rentpgapp/rent_pg_service/model/Booking.java
@@ -1,55 +1,46 @@
 package com.rentpgapp.rent_pg_service.model;
 
-import com.rentpgapp.rent_pg_service.commons.BookingStatus;
+import com.rentpgapp.rent_pg_service.common.BookingStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking {
 
-    @Entity
-    @Table(name = "bookings")
-    public class Booking {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "booking_id")
-        private Long bookingId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booking_id")
+    private Long bookingId;
 /*  Uncomment these private variables once the classes are made */
 
-//        @ManyToOne
-//        @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-//        private Users user;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private Users user;
 //
 //        @ManyToOne
 //        @JoinColumn(name = "room_id", referencedColumnName = "room_id")
 //        private Rooms room;
 
-        @Column(name = "booking_date", nullable = false)
-        private LocalDateTime bookingDate;
+    @Column(name = "booking_date", nullable = false)
+    private LocalDateTime bookingDate;
 
-        @Column(name = "check_in", nullable = false)
-        private LocalDateTime checkIn;
+    @Column(name = "check_in", nullable = false)
+    private LocalDateTime checkIn;
 
-        @Column(name = "check_out", nullable = false)
-        private LocalDateTime checkOut;
+    @Column(name = "check_out", nullable = false)
+    private LocalDateTime checkOut;
 
-        @Enumerated(EnumType.STRING)
-        @Column(name = "status", nullable = false)
-        private BookingStatus status;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private BookingStatus status;
 
-        /*  Uncomment these private variables once the classes are made */
-
-        // Constructors
-        public Booking() {}
-
-        public Booking(LocalDateTime bookingDate,
-                       LocalDateTime checkIn, LocalDateTime checkOut, BookingStatus status) {
-//            this.user = user;
-//            this.room = room;
-            this.bookingDate = bookingDate;
-            this.checkIn = checkIn;
-            this.checkOut = checkOut;
-            this.status = status;
-        }
-
-    }
+    /*  Uncomment these private variables once the classes are made */
+}
 
